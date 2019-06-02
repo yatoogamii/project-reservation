84b1133b3dace29c3613842364621843911514f7
yatoogamii <bracaval.elias.pro@gmail.com>
2019-06-03 00:22:57 +0200

    add button for add command

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,12 @@
-let commandDrink = document.getElementsByClassName('form__select-drink')[0].value;
-document.getElementsByClassName('command__drink')[0].innerHTML = commandDrink;
-let commandTaglieri = document.getElementsByClassName('form__select-taglieri')[0].value;
-document.getElementsByClassName('command__taglieri')[0].innerHTML = commandTaglieri;
-let commandDessert = document.getElementsByClassName('form__select-dessert')[0].value;
-document.getElementsByClassName('command__dessert')[0].innerHTML = commandDessert;
+const commandDrink = document.getElementsByClassName('form__select-drink')[0];
+const commandTaglieri = document.getElementsByClassName('form__select-taglieri')[0];
+const commandDessert = document.getElementsByClassName('form__select-dessert')[0];
+const buttonAddCommand = document.getElementsByClassName('form__button')[0];
+
+function addCommand() {
+  document.getElementsByClassName('command__drink')[0].innerHTML = commandDrink.value;
+  document.getElementsByClassName('command__taglieri')[0].innerHTML = commandTaglieri.value;
+  document.getElementsByClassName('command__dessert')[0].innerHTML = commandDessert.value;
+}
+
+buttonAddCommand.onclick = addCommand;
